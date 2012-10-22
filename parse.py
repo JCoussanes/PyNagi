dafec6d5b1a74c2828f3d34eb5640bfff0ace460
Ythèr <j.coussanes@gmail.com>
2012-10-22 21:43:47 +0200

    new comment

--- parse.py
+++ parse.py
@@ -7,28 +7,29 @@
 
 import re
 
-# Read the file and return all data in a list of dictionary.
+
 def parse_filedat(filename):
-    re_section=re.compile("(\S+)\s+{")
-    re_content=re.compile("(\S+)\s*=\s*(\S+)$")
-    data_list=[] # Create an empty list which will contain all data
+    '''Read the file and return all data in a list of dictionary.'''
+
+    re_section=re.compile("(\S+)\s*{")
+    re_content=re.compile("(\S+)\s*=\s*(\S+)")
+    data_list=[]
     i=-1
 
-    with open(filename,"r") as filedat: # Open the file named "filename" to read only as filedat
+    with open(filename,"r") as filedat:
         for line in filedat:
             line=line.strip()
 
-            section=re_section.match(line) # Check if line matches to re_section
+            section=re_section.match(line)
 
-            # New section
             if section:
+                # New section
                 i+=1
                 data_list.append({"section":section.group(1),"content":{}})
             else:
-                content=re_content.match(line) # Check if the line matches to re_content
-
-            # New content line in the current section
-            if content:
-                data_list[i]["content"][content.group(1)]=content.group(2)
+                # New content line in the current section
+                content=re_content.match(line)
+                if content:
+                    data_list[i]["content"][content.group(1)]=content.group(2)
 
     return l_fichier
