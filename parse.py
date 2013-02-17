c67f505c7a64bd278f9c2eb07eb5658b980680d0
Ythèr <j.coussanes@gmail.com>
2013-02-17 02:44:32 +0100

    variables' name are changed again

--- parse.py
+++ parse.py
@@ -8,16 +8,16 @@
 import re
 
 
-def parse_dat_file(filename):
+def parse_dat_file(file_name):
     '''Read the file and return all data in a list of dictionary'''
 
     re_section=re.compile("(\S+)\s*{")
     re_content=re.compile("(\S+)\s*=\s*(\S+)")
-    data_list=[]
+    l_data=[]
     i=-1
 
-    with open(filename,"r") as filedat:
-        for line in filedat:
+    with open(file_name,"r") as dat_file:
+        for line in dat_file:
             line=line.strip()
 
             section=re_section.match(line)
@@ -25,11 +25,11 @@ def parse_dat_file(filename):
             if section:
                 # New section
                 i+=1
-                data_list.append({"section":section.group(1),"content":{}})
+                l_data.append({"section":section.group(1),"content":{}})
             else:
                 # New content line in the current section
                 content=re_content.match(line)
                 if content:
-                    data_list[i]["content"][content.group(1)]=content.group(2)
+                    l_data[i]["content"][content.group(1)]=content.group(2)
 
-    return data_list
+    return l_data
